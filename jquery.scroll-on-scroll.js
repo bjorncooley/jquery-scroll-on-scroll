045501027490c3eef47c1fe7272bf73bb536b27c
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-23 23:15:31 -0700

    Add functions for animating scroll to next target

--- jquery.scroll-on-scroll.js
+++ jquery.scroll-on-scroll.js
@@ -2,24 +2,62 @@ var lastScrollTop;
 
 $(function(){
 
+    var direction = 'down';
+
     lastScrollTop = 0;
 
-    $(window).scroll(function(){
-
-        getScrollDirection();
-        
-    });
+    $(window).on('scroll', scrollOnScroll);
 });
+
+function scrollOnScroll() {
+
+
+    var windowTop = $(window).scrollTop();
+    var activeScrollSection = $('.scroll-target.active');
+    var scrollTarget = activeScrollSection.next('.scroll-target');
+
+    if ( scrollTarget.length != 0 ) {
+        var scrollTargetTop = scrollTarget.offset().top;
+    }
+    
+
+    direction = getScrollDirection();
+
+    if ( !activeScrollSection.hasClass('animating') ) {
+
+        $(window).off('scroll', scrollOnScroll);
+
+        if ( windowTop < scrollTargetTop && direction == 'down' ) {
+
+            activeScrollSection.addClass('animating');
+
+            $('html, body').stop().animate({ scrollTop: scrollTargetTop}, 1000, function() {
+
+                scrollTarget.addClass('active'); 
+                activeScrollSection.removeClass('active').removeClass('animating');
+                
+                // Clear/wait for residual scroll functions
+                $(window).clearQueue();
+                setTimeout(function(){
+                    $(window).on('scroll', scrollOnScroll);
+                }, 100);                
+
+            });
+        }
+    }
+}
 
 function getScrollDirection() {
 
     var currentScrollTop = $(this).scrollTop();
+    var direction;
 
     if ( currentScrollTop > lastScrollTop ) {
-        console.log("down");
+        direction = 'down';
     } else {
-       console.log("Up");
+       direction = 'up';
     }
 
     lastScrollTop = currentScrollTop;
+    return direction;
 }
