a45c6473852d1817cb62353e3bad7d37322cc0ff
Bjorn Cooley <bjorncooley@gmail.com>
2015-05-23 21:34:07 -0700

    Add function to detect scroll direction

--- jquery.scroll-on-scroll.js
+++ jquery.scroll-on-scroll.js
@@ -1,4 +1,25 @@
+var lastScrollTop;
+
 $(function(){
 
-    
+    lastScrollTop = 0;
+
+    $(window).scroll(function(){
+
+        getScrollDirection();
+        
+    });
 });
+
+function getScrollDirection() {
+
+    var currentScrollTop = $(this).scrollTop();
+
+    if ( currentScrollTop > lastScrollTop ) {
+        console.log("down");
+    } else {
+       console.log("Up");
+    }
+
+    lastScrollTop = currentScrollTop;
+}
